6e8db525f425fdb3ad453c18122588bd1be175ea
jkz1one <mrjoeykeys@gmail.com>
2026-09-18 21:37:25 -0400

    Fix strict typing in pitch launch builder

--- src/gameplay/pitching/pitch_launch_builder.gd
+++ src/gameplay/pitching/pitch_launch_builder.gd
@@ -21,11 +21,11 @@ static func build_nominal(
 		push_error("PitchLaunchBuilder: ball setup/aero profile is missing.")
 		return null
 
-	var parameters := PitchLaunchParameters.new()
+	var parameters: PitchLaunchParameters = PitchLaunchParameters.new()
 	parameters.pitch_id = pitch.id
 	parameters.seed = seed
 
-	var release_offset := pitch.delivery_profile.release_offset_pitcher_frame_m
+	var release_offset: Vector3 = pitch.delivery_profile.release_offset_pitcher_frame_m
 	parameters.position = mound_origin + CoordinateFrame.pitcher_frame_vector(
 		release_offset.x,
 		release_offset.y,
@@ -33,23 +33,23 @@ static func build_nominal(
 		is_left_handed
 	)
 
-	var launch_direction := (target_position - parameters.position).normalized()
+	var launch_direction: Vector3 = (target_position - parameters.position).normalized()
 	parameters.velocity = launch_direction * pitch.nominal_velocity_mps
 
-	var spin_axis_source := pitch.nominal_spin_axis_pitcher_frame
-	var spin_axis_world := CoordinateFrame.pitcher_frame_vector(
+	var spin_axis_source: Vector3 = pitch.nominal_spin_axis_pitcher_frame
+	var spin_axis_world: Vector3 = CoordinateFrame.pitcher_frame_vector(
 		spin_axis_source.x,
 		spin_axis_source.y,
 		spin_axis_source.z,
 		is_left_handed
 	).normalized()
-	var spin_radians_per_second := pitch.nominal_spin_rpm * TAU / 60.0
+	var spin_radians_per_second: float = pitch.nominal_spin_rpm * TAU / 60.0
 	parameters.angular_velocity = spin_axis_world * spin_radians_per_second
 
 	parameters.orientation = Quaternion.IDENTITY
 	parameters.hole_axis_ball_local = pitch.nominal_hole_axis_ball_local.normalized()
 
-	var aero := ball_setup.aero_profile
+	var aero: BallAeroProfileDefinition = ball_setup.aero_profile
 	parameters.mass_kg = aero.mass_kg
 	parameters.radius_m = aero.radius_m
 	parameters.drag_coefficient = aero.drag_coefficient * ball_setup.drag_multiplier
