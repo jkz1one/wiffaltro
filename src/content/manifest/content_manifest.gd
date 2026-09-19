acf9dc30556ecbbb93912741c03c7eff50f5e458
jkz1one <mrjoeykeys@gmail.com>
2026-09-18 21:02:23 -0400

    Phase 0: add content manifest definition

---
+++ src/content/manifest/content_manifest.gd
@@ -0,0 +1,8 @@
+class_name ContentManifest
+extends Resource
+
+@export var pitches: Array[PitchDefinition] = []
+@export var players: Array[PlayerDefinition] = []
+@export var deliveries: Array[DeliveryProfileDefinition] = []
+@export var ball_aero_profiles: Array[BallAeroProfileDefinition] = []
+@export var ball_setups: Array[BallSetupDefinition] = []
