4ece024aa5c39f1d553dec8ede62d8db9523565a
jkz1one <mrjoeykeys@gmail.com>
2026-09-18 21:37:24 -0400

    Fix strict typing in pitch solver

--- src/gameplay/pitching/pitch_flight_solver.gd
+++ src/gameplay/pitching/pitch_flight_solver.gd
@@ -9,24 +9,24 @@ static func step(
 	parameters: PitchLaunchParameters,
 	delta_seconds: float = SUBSTEP_SECONDS
 ) -> void:
-	var acceleration_start := PitchAerodynamics.acceleration(
+	var acceleration_start: Vector3 = PitchAerodynamics.acceleration(
 		state.velocity,
 		state.orientation,
 		state.angular_velocity,
 		parameters
 	)
 
-	var midpoint_velocity := (
+	var midpoint_velocity: Vector3 = (
 		state.velocity
 		+ acceleration_start * delta_seconds * 0.5
 	)
-	var midpoint_orientation := PitchAerodynamics.advance_orientation(
+	var midpoint_orientation: Quaternion = PitchAerodynamics.advance_orientation(
 		state.orientation,
 		state.angular_velocity,
 		delta_seconds * 0.5
 	)
 
-	var acceleration_midpoint := PitchAerodynamics.acceleration(
+	var acceleration_midpoint: Vector3 = PitchAerodynamics.acceleration(
 		midpoint_velocity,
 		midpoint_orientation,
 		state.angular_velocity,
